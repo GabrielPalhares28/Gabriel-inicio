0e9781d7f7f569cf63226eb7998117168884b738
Gabriel Alves Palhares <113656051+GabrielPalhares28@users.noreply.github.com>
2022-11-23 22:07:19 -0300

    Add files via upload
    operações matematicas

---
+++ operadores Aritmeticos/OperadoresAritmeticos.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.operadoresaritmeticos;
+
+/**
+ *
+ * @author 55649
+ */
+public class OperadoresAritmeticos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+         int resultado = 1+2;
+        System.out.println(resultado);
+        resultado = resultado - 1;
+        System.out.println(resultado);
+        
+        resultado = resultado * 2;
+        System.out.println(resultado);
+        
+        resultado = resultado / 2;
+        System.out.println(resultado);
+        
+        resultado = resultado + 8;
+        System.out.println(resultado);
+        
+        resultado = resultado % 7;
+        System.out.println(resultado);
+        
+    }
+}
